35766db1992239a591fd66e9e0ac8e48b4cc29a3
Dobbymin <gmin0701@knu.ac.kr>
2024-07-08 18:30:10 +0900

    feat(interface): style 코드 type 정의

--- src/interface/styleInterface.ts
+++ src/interface/styleInterface.ts
@@ -1,19 +1,50 @@
-export interface IText {
-  fs?: string;
-  fw?: string;
+export interface IStyle {
+  width?: string;
+  height?: string;
+  color?: string;
+  bgColor?: string;
+}
+
+interface IMargin {
+  mgColumn?: string;
+  mgRow?: string;
   mgTop?: string;
   mgBottom?: string;
   mgLeft?: string;
   mgRight?: string;
-  color?: string;
 }
 
-export interface IButton {
-  width?: string;
-  height?: string;
+interface ITypography {
+  fontSize?: string;
+  fontWeight?: string;
+  fontStyle?: string;
+  lineHeight?: string;
+}
+
+export interface IText extends IStyle, IMargin, ITypography {
+  maxWidth?: string;
+}
+
+export interface IButton extends IStyle {
   radius?: string;
-  bgColor?: string;
-  color?: string;
   borderColor?: string;
   children: React.ReactNode;
+}
+
+export interface IContainer extends IStyle, IMargin {
+  gap?: string;
+  textAlign?: string;
+  alignItems?: string;
+  justifyContent?: string;
+  flexDirection?: string;
+}
+
+export interface ITierGraph extends IStyle, IMargin {
+  radius?: string;
+}
+
+export interface ICurrentTierGraph extends IStyle {
+  pdLeft?: string;
+  padding?: string;
+  radius?: string;
 }
