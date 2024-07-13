74d6aeac421fecc11376ed52c6d6fee532216a36
Dobbymin <gmin0701@knu.ac.kr>
2024-07-13 16:55:38 +0900

    Feat(user): 유저 정보 조회 req, res 타입 작성

---
+++ src/apis/user/user.response.ts
@@ -0,0 +1,24 @@
+import { Data } from '@/interface/apis/tier';
+import { UserData } from '@/interface/apis/user';
+
+export interface UserInfoResponse {
+  result: string;
+  data: {
+    totalPage: number;
+    hasNext: boolean;
+    data: UserData[];
+  };
+  message: string;
+  errorCode: string;
+}
+
+export interface FixUserInfoResponse {
+  nickname: string;
+}
+
+export interface UserStrickResponse {
+  result: string;
+  data: Data[];
+  message: string;
+  errorCode: string;
+}
