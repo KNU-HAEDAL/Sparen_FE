1e720d32c3b2229ccb996de4121611bf784a7613
Dobbymin <gmin0701@knu.ac.kr>
2024-07-14 13:59:43 +0900

    Feat(login): 리다이렉트 코드 작성

--- src/pages/login/components/RedirectPage.tsx
+++ src/pages/login/components/RedirectPage.tsx
@@ -1,5 +1,5 @@
-import { AuthAPI } from '../../apis/auth/auth.api';
-import { BASE_URI } from '../../constants/URI';
+import { AuthAPI } from '@/apis/auth/auth.api';
+import { BASE_URI } from '@/constants/URI';
 
 const RedirectPage = () => {
   const code = new URLSearchParams(window.location.search).get('code');
