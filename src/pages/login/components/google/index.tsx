49dfc7fb7c8b5d2a9817b209fdf5783033b68e01
Dobbymin <gmin990624@gmail.com>
2024-07-22 19:45:24 +0900

    feat(login): 소셜 로그인 미구현 기능 처리

--- src/pages/login/components/google/index.tsx
+++ src/pages/login/components/google/index.tsx
@@ -1,13 +1,14 @@
 import Logo from '@/assets/login/google.png';
-import { KAKAO_AUTH_URL } from '@/constants/URI.ts';
+// import { KAKAO_AUTH_URL } from '@/constants/URI.ts';
 import * as Base from '@/styles/baseStyles';
 
 const GoogleLogin = () => {
   const handlerLogin = () => {
-    window.location.href = KAKAO_AUTH_URL;
+    // window.location.href = KAKAO_AUTH_URL;
+    alert('준비중입니다.');
   };
   return (
-    <Base.Container cursor='pointer'>
+    <Base.Container onClick={handlerLogin} cursor='pointer'>
       <Base.Img
         width='3rem'
         textAlign='center'
