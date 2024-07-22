873999dfe0052c10c0d7c43e1b9e701f23c29548
Dobbymin <gmin0701@knu.ac.kr>
2024-07-22 15:45:47 +0900

    Feat(challenge): 파라미터 값 type 지정

--- src/apis/challenge/list-challenge/list.challenge.api.ts
+++ src/apis/challenge/list-challenge/list.challenge.api.ts
@@ -1,7 +1,13 @@
 import { axiosClient } from '@/apis/AxiosClient';
 
+type params = {
+  page: number;
+  size: number;
+  category: string;
+};
+
 // 카테고리 버튼 눌렀을때 -> 해당 페이지 호출
-export async function getChallengeList(page, size, category) {
+export async function getChallengeList({ page, size, category }: params) {
   try {
     const response = await axiosClient.get(`/api/challengeGroups`, {
       params: { page, size, category },
