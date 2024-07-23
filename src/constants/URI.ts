3fbfbdd983606b0ef2f6e3966607bd6f4185eb99
Dobbymin <gmin0701@knu.ac.kr>
2024-07-23 17:18:31 +0900

    Fix(uri): uri 경로 최종 수정 및 네이버 url 주석처리

--- src/constants/URI.ts
+++ src/constants/URI.ts
@@ -6,4 +6,4 @@ export const GOGGLE_REST_API_KEY = import.meta.env.VITE_APP_GOOGLE_API_KEY;
 export const REDIRECT_URI = import.meta.env.VITE_APP_REDIRECT_URI;
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
-export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`;
+// export const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`;
