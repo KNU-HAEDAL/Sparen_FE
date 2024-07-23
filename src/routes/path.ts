1efb4096ff33ec005fe946a0f49379caaac4524b
Dobbymin <gmin990624@gmail.com>
2024-07-24 04:20:53 +0900

    feat(router): 경로 상수화

---
+++ src/routes/path.ts
@@ -0,0 +1,12 @@
+export const RouterPath = {
+  root: '/',
+  main: '/',
+  challenge: 'challenge',
+  shorts: 'shorts',
+  rank: 'rank',
+  dashboard: 'dashboard',
+  login: 'login',
+  redirect: 'redirect',
+  auth: 'auth',
+  register: 'register',
+};
