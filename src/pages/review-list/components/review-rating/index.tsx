3ece1af0766cfdc4bbdeb8d2a2424f42ee30f3d6
Dobbymin <gmin0701@knu.ac.kr>
2024-08-05 17:54:01 +0900

    Feat(review): 리뷰 페이지 구현 및 컴포넌트 분리

---
+++ src/pages/review-list/components/review-rating/index.tsx
@@ -0,0 +1,157 @@
+import { useState, useEffect } from 'react';
+
+import { getChallegeAvgScore } from '@/apis/review/review.api';
+import { Box, Text } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+const ReviewRating = ({ props }) => {
+  const [datas, setDatas] = useState(null);
+  const [starAvg, setStarAvg] = useState(0);
+  const [ratingToPercent, setRatingToPercent] = useState({
+    width: `${(starAvg / 5) * 100}%`,
+  });
+
+  useEffect(() => {
+    getChallegeAvgScore(1).then((res) => {
+      setDatas(res.ratingCount);
+      console.log('ddd', res.ratingCount);
+      setStarAvg(res.averageRating);
+      setRatingToPercent({ width: `${(res.averageRating / 5) * 100}%` });
+    });
+  }, []);
+
+  return (
+    <>
+      {datas ? (
+        <StarBox>
+          <CWrapper style={{ alignItems: 'center' }}>
+            <Text fontSize='var(--font-size-xxl)' fontWeight='700'>
+              {starAvg}
+            </Text>
+            <StarRating>
+              <StarFill style={ratingToPercent}>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+              </StarFill>
+              <StarBase>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+              </StarBase>
+            </StarRating>
+          </CWrapper>
+          <VLine />
+          <RWrapper>
+            <CWrapper>
+              <SubText>매우 만족</SubText>
+              <SubText>만족</SubText>
+              <SubText>보통</SubText>
+              <SubText>별로</SubText>
+              <SubText>매우 별로</SubText>
+            </CWrapper>
+            <CWrapper>
+              <Bar percentage={`${datas[1] * 100}%`} />
+              <Bar percentage={`${datas[2] * 100}%`} />
+              <Bar percentage={`${datas[3] * 100}%`} />
+              <Bar percentage={`${datas[4] * 100}%`} />
+              <Bar percentage={`${datas[5] * 100}%`} />
+            </CWrapper>
+            <CWrapper>
+              <SubText>{datas[1]}</SubText>
+              <SubText>{datas[2]}</SubText>
+              <SubText>{datas[3]}</SubText>
+              <SubText>{datas[4]}</SubText>
+              <SubText>{datas[5]}</SubText>
+            </CWrapper>
+          </RWrapper>
+        </StarBox>
+      ) : (
+        <div />
+      )}
+    </>
+  );
+};
+
+export default ReviewRating;
+
+const StarBox = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  align-self: center;
+  width: 90%;
+  border-radius: 10px;
+  background-color: var(--color-green-06);
+  padding: 0px 20px;
+  justify-content: center;
+`;
+
+export const VLine = styled.div`
+  border: #d7d7d7 solid 0.5px;
+  height: 100px;
+  border-radius: 10px;
+  margin: 10px;
+`;
+
+export const RWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-shrink: 0;
+  align-items: center;
+  width: 170px;
+  justify-content: space-between;
+`;
+
+export const CWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+`;
+
+export const SubText = styled(Text)`
+  font-size: var(--font-size-xs);
+  color: var(--color-grey-01);
+  margin: 0.2px;
+`;
+
+export const StarRating = styled.div`
+  position: relative;
+  unicode-bidi: bidi-override;
+  width: max-content;
+  -webkit-text-fill-color: transparent;
+  -webkit-text-stroke-width: 0.8px;
+  -webkit-text-stroke-color: var(--color-green-01);
+`;
+export const StarFill = styled.div`
+  padding: 0;
+  position: absolute;
+  z-index: 1;
+  display: flex;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  -webkit-text-fill-color: var(--color-green-01);
+`;
+export const StarBase = styled.div`
+  z-index: 0;
+  padding: 0;
+`;
+
+export const Bar = styled.div`
+  border-radius: 10px;
+  height: 5px;
+  --percentage: ${(props) => `${props.percentage}`};
+  background: linear-gradient(
+    to right,
+    var(--color-green-01) var(--percentage),
+    #d7d7d7 var(--percentage)
+  );
+  width: 80px;
+  flex-shrink: 0;
+  margin: 6px 0;
+`;
---
+++ src/pages/review-write/index.tsx
@@ -0,0 +1,197 @@
+import { useState } from 'react';
+import { PiStarFill, PiStarLight } from 'react-icons/pi';
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from 'antd';
+
+import { postReview } from '@/apis/review/review.api';
+import TopBar from '@/components/features/layout/top-bar';
+import { Box, Text } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+const ReviewWrite = () => {
+  const Item1 = ['어려워요', '적당해요', '쉬워요'];
+  const Item2 = ['뿌듯해요', '유익해요', '애매해요'];
+  const navigate = useNavigate();
+
+  const [rating, setRating] = useState(0);
+  const [selectedItem1, setSelectedItem1] = useState(null);
+  const [selectedItem2, setSelectedItem2] = useState(null);
+  const [text, setText] = useState('');
+
+  const handleItem1Click = (item) => {
+    setSelectedItem1(item);
+  };
+  const handleItem2Click = (item) => {
+    setSelectedItem2(item);
+  };
+
+  const saveHandler = () => {
+    postReview(9, text, rating)
+      .then((res) => {
+        console.log('응답: ', res);
+        alert('성공적으로 저장했습니다.');
+        navigate('/');
+      })
+      .catch(() => {
+        alert('저장에 실패했습니다.');
+        navigate('/');
+      });
+  };
+
+  return (
+    <>
+      <TopBar title='챌린지 리뷰' backgroundColor='#fff' type='Page' />
+      <ReviewWriteLayout>
+        <Text fontSize='var(--font-size-lg)' fontWeight='700'>
+          길에 떨어진 쓰레기 줍기 챌린지
+        </Text>
+        <Wrapper margin='20px 0' alignItems='end' alignSelf='center'>
+          {[...Array(rating)].map((a, i) => (
+            <PiStarFill
+              size='35'
+              key={i}
+              onClick={() => setRating(i + 1)}
+              color='var(--color-green-01)'
+            />
+          ))}
+          {[...Array(5 - rating)].map((a, i) => (
+            <PiStarLight
+              size='35'
+              key={i}
+              onClick={() => setRating(rating + i + 1)}
+              color='var(--color-green-01)'
+            />
+          ))}
+          <Star>{rating}.0</Star>
+          <Text
+            fontSize='var(--font-size-sm)'
+            color='var(--color-gray-01)'
+            margin='3px'
+          >
+            /5.0
+          </Text>
+        </Wrapper>
+        <Wrapper margin='10px 0'>
+          <Text
+            fontSize='var(--font-size-md)'
+            fontWeight='700'
+            marginLeft='5px'
+            marginRight='20px'
+          >
+            난이도
+          </Text>
+          {Item1.map((item) => (
+            <CheckButton
+              key={item}
+              onClick={() => handleItem1Click(item)}
+              isSelected={selectedItem1 === item}
+            >
+              {item}
+            </CheckButton>
+          ))}
+        </Wrapper>
+        <Wrapper margin='10px 0'>
+          <Text
+            fontSize='var(--font-size-md)'
+            fontWeight='700'
+            marginLeft='5px'
+            marginRight='20px'
+          >
+            성취감
+          </Text>
+          {Item2.map((item) => (
+            <CheckButton
+              key={item}
+              onClick={() => handleItem2Click(item)}
+              isSelected={selectedItem2 === item}
+            >
+              {' '}
+              {item}
+            </CheckButton>
+          ))}
+        </Wrapper>
+        <Text
+          fontSize='var(--font-size-md)'
+          fontWeight='700'
+          marginLeft='5px'
+          marginRight='20px'
+          marginTop='20px'
+        >
+          리뷰 쓰기
+        </Text>
+        <InputArea
+          placeholder='챌린지 후 느낀점을 적어주세요'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <SubmitButton onClick={saveHandler}>등록하기</SubmitButton>
+      </ReviewWriteLayout>
+    </>
+  );
+};
+
+export default ReviewWrite;
+
+const ReviewWriteLayout = styled.div`
+  position: relative;
+  margin: 30px;
+  margin-bottom: 60px;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+`;
+
+const Wrapper = styled(Box)`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Star = styled.div`
+  font-size: var(--font-size-xxl);
+  font-weight: bold;
+  margin-left: 10px;
+`;
+
+export const InputArea = styled.textarea`
+  font-size: var(--font-size-sm);
+  border-radius: 20px;
+  border: var(--color-green-01) 1px solid;
+  padding: 10px;
+  height: 30vh;
+  resize: none;
+  margin-top: 10px;
+`;
+
+export const SubmitButton = styled(Button)`
+  position: fixed;
+  display: block;
+  bottom: 60px;
+  width: calc(100% - 60px);
+  height: 50px;
+  margin-top: 30px;
+  border-radius: 20px;
+  background-color: var(--color-green-01);
+  color: var(--color-white);
+  font-size: var(--font-size-md);
+  font-weight: bold;
+  border: none;
+`;
+
+export const CheckButton = styled.button`
+  height: 25px;
+  border-radius: 20px;
+  margin-left: 5px;
+  border: var(--color-grey-02) 1px solid;
+  background-color: var(--color-white);
+  color: var(--color-grey-02);
+  font-size: var(--font-size-sm);
+  text-align: center;
+  flex-shrink: 0;
+  ${({ isSelected }) =>
+    isSelected &&
+    `
+        border: var(--color-green-01) 1px solid;
+        color: var(--color-green-01);
+    `}
+`;
