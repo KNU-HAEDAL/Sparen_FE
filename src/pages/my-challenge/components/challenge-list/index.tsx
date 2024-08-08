9c9c02f6be3982a0c434cbaf7924918bdc92092e
Dobbymin <gmin0701@knu.ac.kr>
2024-08-08 16:34:24 +0900

    Refactor(my-challenge): 코드 정리 및 디자인 코드 수정

--- src/pages/my-challenge/components/challenge-list/index.tsx
+++ src/pages/my-challenge/components/challenge-list/index.tsx
@@ -1,8 +1,9 @@
 import * as S from './styles';
 import FinishStamp from '@/assets/challenge/ZZAN-Black.png';
-import * as Base from '@/styles/baseStyles';
+import { Box, Image, Text } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 
-interface ChallengeListProps {
+type ChallengeListProps = {
   BackgroundColor: string;
   color: string;
   BorderColor: string;
@@ -10,39 +11,51 @@ interface ChallengeListProps {
     id: number;
     title: string;
   }[];
-}
+};
 
-const ChallengeList: React.FC<ChallengeListProps> = ({
+const ChallengeList = ({
   BackgroundColor,
   color,
   BorderColor,
   challenges,
-}) => {
+}: ChallengeListProps) => {
   return (
     <>
-      <S.ChallengeListLayout>
+      <ChallengeListBox>
         {challenges.map((challenge) => (
-          <S.ChallengeContainer key={challenge.id}>
+          <Box
+            display='flex'
+            marginLeft='1rem'
+            width='100%'
+            justifyContent='space-between'
+            gap='10px'
+            alignItems='center'
+            flexDirection='row'
+            key={challenge.id}
+          >
             <S.ChallengeImgContainer>
-              <S.ChallengeImg src={FinishStamp} />
+              <Image
+                filter='opacity(0.5) drop-shadow(0 0 0 #c0c0c0)'
+                src={FinishStamp}
+              />
             </S.ChallengeImgContainer>
-
-            <S.ChallengeInfoContainer>
-              <Base.Text
-                fontSize='1.125rem'
-                fontStyle='normal'
-                fontWeight='700'
-                lineHeight='normal'
-              >
+            <Box display='flex' margin='1rem 0'>
+              <Text fontSize='1.2rem' fontStyle='normal' fontWeight='700'>
                 {challenge.title}
-              </Base.Text>
-            </S.ChallengeInfoContainer>
-
-            <S.ReviewContainer
-              bgColor={BackgroundColor}
+              </Text>
+            </Box>
+            <Box
+              display='flex'
+              padding='1rem 0.625rem'
+              justifyContent='center'
+              alignItems='center'
+              height='1rem'
+              borderRadius='0.625rem'
+              backgroundColor={BackgroundColor}
               borderColor={BorderColor}
+              border='1px solid'
             >
-              <Base.Text
+              <Text
                 fontSize='0.875rem'
                 fontStyle='normal'
                 fontWeight='700'
@@ -50,18 +63,24 @@ const ChallengeList: React.FC<ChallengeListProps> = ({
                 color={color}
               >
                 리뷰 쓰기
-              </Base.Text>
-            </S.ReviewContainer>
-          </S.ChallengeContainer>
+              </Text>
+            </Box>
+          </Box>
         ))}
-        <S.ChallengeBarContainer>
-          <S.ChallengeBar>
-            <S.ChallengeBarFill />
-          </S.ChallengeBar>
-        </S.ChallengeBarContainer>
-      </S.ChallengeListLayout>
+      </ChallengeListBox>
     </>
   );
 };
 
 export default ChallengeList;
+
+const ChallengeListBox = styled(Box)`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  // border-bottom: 1px solid #d4d6dd;
+  background-color: #fff;
+`;
