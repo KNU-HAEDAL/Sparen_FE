45e1c6cb925911f77c60fbd8697338f509a070a7
Dobbymin <gmin0701@knu.ac.kr>
2024-08-08 16:33:13 +0900

    Refactor(my-challenge): css 코드 통합 및 chakra 라이브러리

--- src/pages/my-challenge/index.tsx
+++ src/pages/my-challenge/index.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
 
 import ChallengeList from './components/challenge-list';
-import * as S from './styles';
 import { getCurrentChallengeList } from '@/apis/challenge/my-challenge/my.challenge.api';
 import TopBar from '@/components/features/layout/top-bar';
 import { ChallengeData } from '@/interface/apis/challenge';
-import * as Base from '@/styles/baseStyles';
+import { Box, Text } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 
 // interface ChallengeDataType {
 //   id: number;
@@ -32,25 +32,35 @@ const MyChallengePage = () => {
   return (
     <>
       <TopBar type='Page' title='내 챌린지' backgroundColor='#fff' />
-      <Base.Text
+      <Text
         fontSize='var(--font-size-xxl)'
         fontWeight='700'
-        mgLeft='1rem'
-        mgBottom='1rem'
-        mgTop='1rem'
+        marginLeft='1rem'
+        marginBottom='1rem'
+        marginTop='1rem'
+        width='100%'
       >
         참여중인 챌린지
-      </Base.Text>
-      <S.MyChallengeLayout>
+      </Text>
+      <ChallengeListBox>
         <ChallengeList
           BackgroundColor='#fff'
           color='#c0c0c0'
           BorderColor='#c0c0c0'
           challenges={listChallenges}
         />
-      </S.MyChallengeLayout>
+      </ChallengeListBox>
     </>
   );
 };
 
 export default MyChallengePage;
+
+const ChallengeListBox = styled(Box)`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  flex-direction: column;
+  padding: 0 1rem;
+  background-color: #fff;
+`;
