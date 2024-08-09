c1f6df0952ef73d28b2a96fa05b71832db49cf43
Dobbymin <gmin0701@knu.ac.kr>
2024-08-09 21:56:45 +0900

    Feat(dashboard): 완료한 챌린지 목록 이동 기능 구현

--- src/pages/dashboard/components/challenge-record/index.tsx
+++ src/pages/dashboard/components/challenge-record/index.tsx
@@ -1,25 +1,38 @@
-// import { useNavigate } from 'react-router-dom';
-import * as S from './styles';
-import * as Base from '@/styles/baseStyles';
+import { RouterPath } from '@/routes/path';
+import { Text } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 
 const ChallengeRecord = () => {
-  // const navigate = useNavigate();
-  const handlerPage = () => {
-    // navigate('/challenge/record-list');
-    console.log('click');
-  };
   return (
     <>
-      <Base.Text mgLeft='1rem' mgTop='2rem'>
+      <Text
+        marginLeft='1rem'
+        marginTop='2rem'
+        fontWeight='700'
+        fontSize='var(--font-size-xl)'
+      >
         내기록
-      </Base.Text>
-      <S.FinishChallengeBtn>
-        <S.FinishChallengeText onClick={handlerPage}>
-          완료한 챌린지 목록
-        </S.FinishChallengeText>
-      </S.FinishChallengeBtn>
+      </Text>
+      <ChallengeRecordListButton
+        href={`${RouterPath.challenge}/${RouterPath.myRecord}`}
+      >
+        완료한 챌린지 목록
+      </ChallengeRecordListButton>
     </>
   );
 };
 
 export default ChallengeRecord;
+
+const ChallengeRecordListButton = styled.a`
+  display: flex;
+  padding: 0.75rem 6rem 0.5rem 6rem;
+  justify-content: center;
+  border-radius: 1.25rem;
+  margin: 1rem 1rem 2rem 1rem;
+  cursor: pointer;
+  background-color: var(--color-green-01);
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  font-size: var(--font-size-xl);
+  color: #fff;
+`;
