b03ada53c207ac58b7b05602f9343dc4a273bd03
Dobbymin <gmin0701@knu.ac.kr>
2024-08-10 13:29:04 +0900

    Fix(provider): Auth provider 코드 수정

--- src/provider/auth/index.tsx
+++ src/provider/auth/index.tsx
@@ -4,34 +4,50 @@ import { createContext, useContext, useEffect, useState } from 'react';
 import { authLocalStorage } from '@/utils/storage';
 
 type AuthInfo = {
-  id: string;
-  nickname: string;
   token: string;
 };
 
-export const AuthContext = createContext<AuthInfo | undefined>(undefined);
+type AuthContextData = {
+  authInfo: AuthInfo | undefined;
+  setAuthInfo: (authInfo: AuthInfo) => void;
+  // clearAuthInfo: () => void;
+};
+
+export const AuthContext = createContext<AuthContextData | undefined>(
+  undefined
+);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const currentAuthToken = authLocalStorage.get();
   const [isReady, setIsReady] = useState(!currentAuthToken);
-
   const [authInfo, setAuthInfo] = useState<AuthInfo | undefined>(undefined);
+
+  const handleAuthInfo = (currentAuthInfo: AuthInfo) => {
+    setAuthInfo(currentAuthInfo);
+    authLocalStorage.set(currentAuthInfo.token);
+  };
 
   useEffect(() => {
     if (currentAuthToken) {
       setAuthInfo({
-        id: currentAuthToken,
-        nickname: currentAuthToken,
         token: currentAuthToken,
       });
       setIsReady(true);
     }
   }, [currentAuthToken]);
 
-  if (!isReady) return <></>;
+  if (!isReady) return null;
   return (
-    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ authInfo, setAuthInfo: handleAuthInfo }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = (): AuthContextData => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
