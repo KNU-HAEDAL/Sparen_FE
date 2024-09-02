1fa362e7c6f2321bcb902af1a6784710c339b4a9
Dobbymin <gmin0701@knu.ac.kr>
2024-09-02 14:42:16 +0900

    Feat(api): 토큰 재발급 및 Refresh Token 기능 구현

--- src/apis/AxiosClient.ts
+++ src/apis/AxiosClient.ts
@@ -1,15 +1,19 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios, {
+  AxiosInstance,
+  AxiosRequestConfig,
+  InternalAxiosRequestConfig,
+} from 'axios';
 
 import { BASE_URL } from '@/constants/URI';
 import { QueryClient } from '@tanstack/react-query';
 
 const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
   const instance = axios.create({
-    timeout: 10000,
+    timeout: 5000,
     ...config,
     headers: {
       Accept: 'application/json',
-      'Content-Type': 'application/json',
+      // 'Content-Type': 'application/json',
       'Cross-Control-Allow-Origin': '*',
       ...config.headers,
     },
@@ -43,10 +47,12 @@ export const multiPartClient = axios.create({
 });
 
 axiosClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('accessToken');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+  (config: InternalAxiosRequestConfig) => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken !== undefined) {
+      config.headers['Content-Type'] = 'application/json';
+      config.headers.Authorization = `Bearer ${accessToken}`;
+      // config.headers['Authorization'] = `Bearer ${accessToken}`;
     }
     return config;
   },
@@ -55,42 +61,37 @@ axiosClient.interceptors.request.use(
   }
 );
 
-// axiosClient.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   async (error) => {
-//     const originalRequest = error.config;
-//     if (error.response.status === 401 && !originalRequest._retry) {
-//       originalRequest._retry = true;
-//       const refreshToken = secureLocalStorage.getItem('refreshToken');
-//       if (!refreshToken) {
-//         return Promise.reject(error);
-//       }
-//       const resp = await fetch(
-//         `${import.meta.env.VITE_API_BASE_URL}/api/auth/refresh`,
-//         {
-//           method: 'post',
-//           headers: {
-//             'Content-Type': 'application/json',
-//             'Cross-Control-Allow-Origin': '*',
-//             Authorization: `Bearer ${refreshToken}`,
-//           },
-//         }
-//       );
-//       if (resp.ok) {
-//         console.log('토큰 재발급 성공');
-//         const data = await resp.json();
-//         secureLocalStorage.setItem('accessToken', data.accessToken);
-//         secureLocalStorage.setItem('refreshToken', data.refreshToken);
-//         return axiosClient(originalRequest);
-//       } else {
-//         console.log('토큰 재발급 실패');
-//         secureLocalStorage.removeItem('accessToken');
-//         secureLocalStorage.removeItem('refreshToken');
-//       }
-//       return Promise.reject(error);
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+axiosClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const refreshToken = localStorage.getItem('refreshToken');
+      if (!refreshToken) {
+        return Promise.reject(error);
+      }
+      const resp = await fetch(`${BASE_URL}/api/auth/refresh`, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'Cross-Control-Allow-Origin': '*',
+          Authorization: `Bearer ${refreshToken}`,
+        },
+      });
+      if (resp.ok) {
+        console.log('토큰 재발급 성공');
+        const data = await resp.json();
+        localStorage.setItem('accessToken', data.accessToken);
+        localStorage.setItem('refreshToken', data.refreshToken);
+        return axiosClient(originalRequest);
+      } else {
+        console.log('토큰 재발급 실패');
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+      }
+      return Promise.reject(error);
+    }
+    return Promise.reject(error);
+  }
+);
