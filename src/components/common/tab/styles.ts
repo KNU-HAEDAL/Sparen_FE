1f932866737de33e8f894ae5c8fbd695c09bda27
joojjang <itchu999@naver.com>
2024-09-21 19:16:48 +0900

    design(tab): 탭 배경색 변경 및 그림자 삭제

--- src/components/common/tab/styles.ts
+++ src/components/common/tab/styles.ts
@@ -1,7 +1,5 @@
-import { css, keyframes } from '@emotion/react';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-
-// 챌린지 상세 페이지 css
 
 export const TabsContainer = styled.div`
   display: flex;
@@ -21,23 +19,6 @@ export const TabPanelContainer = styled.div`
   position: relative;
   top: 50px;
   text-align: center;
-`;
-
-export const Image = styled.img`
-  position: relative;
-  margin: auto;
-  display: block;
-  height: 40%;
-  opacity: 20%;
-  object-fit: cover;
-  margin-bottom: 28px;
-  filter: grayscale(100%);
-`;
-
-export const ImageMask = styled.div`
-  background-color: var(--color-green-06);
-  position: relative;
-  top: 50px;
 `;
 
 export const Wrapper = styled.div``;
@@ -63,10 +44,9 @@ export const StylizedTab = styled.div<StylizedTabProps>`
   ${(p) =>
     p.active &&
     css`
-      color: var(--color-white);
-      font-weight: bold;
+      color: var(--color-green-01);
+      font-weight: 600;
       border-radius: 20px;
-      animation: ${inset} 0.4s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
     `}
 `;
 
@@ -107,20 +87,9 @@ export const TabSlider = styled.div<TabSliderProps>`
   position: absolute;
   top: 3px;
   height: 50px;
-  background-color: var(--color-green-01);
+  background-color: var(--color-white);
   border-radius: 20px;
   transition: 0.2s;
   transform: ${({ width, index }) => `translateX(${width * index}px)`};
   width: ${({ width }) => `${width}px`};
 `;
-
-const inset = keyframes`
-    0% {
-      -webkit-box-shadow: 0 0 0 0 rgba(92, 198, 186, 0);
-              box-shadow: 0 0 0 0 rgba(92, 198, 186, 0);
-    }
-    100% {
-      -webkit-box-shadow: 3px 3px 3px rgba(92, 198, 186, 0.5);
-              box-shadow: 3px 3px 3px rgba(92, 198, 186, 0.5);
-    }
-  `;
