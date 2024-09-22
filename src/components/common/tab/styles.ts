144c75ed49ffd143f03dbc1a3ac6aa219b2a490a
joojjang <itchu999@naver.com>
2024-09-22 16:33:47 +0900

    remove(tab): 필요 없는 코드 삭제

--- src/components/common/tab/styles.ts
+++ src/components/common/tab/styles.ts
@@ -1,26 +1,6 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
-export const TabsContainer = styled.div`
-  display: flex;
-  position: relative;
-  top: 50px;
-  align-self: center;
-  width: 90%;
-  margin: 0 auto;
-  height: 55px;
-  border-radius: 20px;
-  background-color: var(--color-green-06);
-`;
-
-export const TabPanelContainer = styled.div`
-  height: 100%;
-  width: 100%;
-  position: relative;
-  top: 50px;
-  text-align: center;
-`;
-
 export const StylizedTab = styled.div<{
   active?: boolean;
   onClick?: () => void;
