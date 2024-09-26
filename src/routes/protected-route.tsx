2f85e681c00b5b1d736260ca02f3dd9d8ca3e0e0
joojjang <itchu999@naver.com>
2024-09-26 23:07:06 +0900

    docs(getDynamicPath): 주석 수정

--- src/routes/protected-route.tsx
+++ src/routes/protected-route.tsx
@@ -25,7 +25,7 @@ export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
 // 로그인 필요한 페이지라면 로그인 페이지로 리다이렉트, 로그인 완료 시 원래 페이지로 돌아가는 함수
 export const getDynamicPath = {
   login: (redirect?: string) => {
-    const currentRedirect = redirect ?? window.location.pathname; // window.location 사용, host 뺀 경로로 접근
+    const currentRedirect = redirect ?? window.location.pathname; // host 뺀 경로(pathname)로 접근
     return `/${RouterPath.auth}?redirect=${encodeURIComponent(currentRedirect)}`;
   },
 };
