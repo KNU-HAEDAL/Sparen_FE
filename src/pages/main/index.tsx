a427aaab9e35071f6e07841cbebe4332dedd0538
joojjang <itchu999@naver.com>
2024-09-29 05:01:25 +0900

    fix(main): 높이 수정

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -2,7 +2,7 @@ import Category from './components/category';
 import Review from './components/review';
 import Strick from './components/strick';
 import Tier from './components/tier';
-import TopBar from '@/components/features/layout/top-bar';
+import TopBar, { HEADER_HEIGHT } from '@/components/features/layout/top-bar';
 import styled from '@emotion/styled';
 
 const MainPage = () => {
@@ -22,5 +22,5 @@ const MainPage = () => {
 export default MainPage;
 
 const Wrapper = styled.div`
-  min-height: 100vh;
+  min-height: calc(100vh - ${HEADER_HEIGHT});
 `;
