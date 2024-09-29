5faa67916fb44d72faf24ee1d1a76faf5934fd28
Dobbymin <gmin0701@knu.ac.kr>
2024-09-29 13:43:35 +0900

    Feat(review): 내가 참여한 챌린지 기록 list 보기 구현

--- src/pages/my-challenge-record/components/list-item.tsx
+++ src/pages/my-challenge-record/components/list-item.tsx
@@ -2,18 +2,25 @@ import ProfileImg from '@/assets/challenge/ZZAN-Green.png';
 import { Image, Text } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 
-const ListItem = () => {
+type Props = {
+  challengeTitle: string;
+  userNickname: string;
+  profileImageUrl?: string | null;
+};
+
+const ListItem = ({ challengeTitle, profileImageUrl }: Props) => {
   return (
     <ListItemLayout>
       <ProfileContainer>
-        <Image src={ProfileImg} alt='profile' width='1.5rem' />
+        <Image
+          src={profileImageUrl || ProfileImg}
+          alt='profile'
+          width='1.5rem'
+        />
       </ProfileContainer>
-      <ListText maxWidth='150px' fontWeight='700' fontSize='1.125rem'>
-        쓰레기 줍기 챌린지
+      <ListText fontWeight='700' fontSize='1.125rem'>
+        {challengeTitle}
       </ListText>
-      <Text maxWidth='70px' fontSize='0.8rem'>
-        2024.08.05
-      </Text>
     </ListItemLayout>
   );
 };
@@ -25,10 +32,12 @@ const ListItemLayout = styled.div`
   flex-direction: row;
   width: 100%;
   gap: 20px;
-  justify-content: space-between;
   align-items: center;
   border-bottom: 1px solid #e0e0e0;
   margin: 0.2rem 1rem;
+  &:last-child {
+    border-bottom: none;
+  }
 `;
 
 const ProfileContainer = styled.div`
@@ -47,5 +56,4 @@ const ListText = styled(Text)`
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-  /* max-width: 150px; */
 `;
