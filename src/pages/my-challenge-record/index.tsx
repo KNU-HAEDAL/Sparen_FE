798505fef81545948b59f55c1ab51959840980c3
Dobbymin <gmin0701@knu.ac.kr>
2024-09-29 13:44:24 +0900

    Feat(review): 내가 참여한 챌린지 기록 list infinity scroll 구현

--- src/pages/my-challenge-record/index.tsx
+++ src/pages/my-challenge-record/index.tsx
@@ -1,9 +1,48 @@
+import { useCallback, useEffect, useState } from 'react';
+
 import ListItem from './components/list-item';
+import { useGetReview } from '@/apis/my-challenge-record/getReview.api';
+import { ChallengeData } from '@/apis/my-challenge-record/getReview.response';
 import TopBar from '@/components/features/layout/top-bar';
-import { Box } from '@chakra-ui/react';
+import { Box, Spinner, Text } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 
 const MyChallengeRecord = () => {
+  const [page, setPage] = useState(0);
+  const [allChallenges, setAllChallenges] = useState<ChallengeData[]>([]);
+  const { data, isLoading } = useGetReview(page, 20);
+
+  const loadMoreChallenges = useCallback(() => {
+    if (data?.data.hasNext && !isLoading) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  }, [data, isLoading]);
+
+  useEffect(() => {
+    if (data?.data.data) {
+      setAllChallenges((prevChallenges) => [
+        ...prevChallenges,
+        ...data.data.data,
+      ]);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop >=
+        document.documentElement.offsetHeight - 200
+      ) {
+        loadMoreChallenges();
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [loadMoreChallenges]);
+
+  // const challenges = data?.data.data || [];
+
   return (
     <>
       <TopBar
@@ -12,11 +51,28 @@ const MyChallengeRecord = () => {
         backgroundColor='var(--color-green-06)'
       />
       <MyChallengeRecordLayout>
+        <Box textAlign='center' marginY={3}>
+          <Text fontWeight='600'>챌린지 기록을 눌러 리뷰를 작성해보세요!</Text>
+        </Box>
         <ChallengeList>
-          <ListItem />
-          <ListItem />
-          <ListItem />
+          {allChallenges.length > 0 ? (
+            allChallenges.map((challenge) => (
+              <ListItem
+                key={challenge.challengeId}
+                challengeTitle={challenge.challengeTitle}
+                userNickname={challenge.user.nickname}
+                profileImageUrl={challenge.user.profileImageUrl}
+              />
+            ))
+          ) : (
+            <Text>챌린지 기록이 존재하지 않습니다.</Text>
+          )}
         </ChallengeList>
+        {isLoading && (
+          <SpinnerContainer>
+            <Spinner size='xl' />
+          </SpinnerContainer>
+        )}
       </MyChallengeRecordLayout>
     </>
   );
@@ -27,8 +83,8 @@ export default MyChallengeRecord;
 const MyChallengeRecordLayout = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
   background-color: var(--color-green-06);
+  padding-bottom: 5rem;
 `;
 
 const ChallengeList = styled(Box)`
@@ -37,7 +93,13 @@ const ChallengeList = styled(Box)`
   padding: 0.625rem 1.5rem;
   flex-direction: column;
   align-items: center;
-
   border-radius: 1.25rem;
   background-color: #fff;
+`;
+
+const SpinnerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 `;
