decfa47905300e635d7285ac752c46a825cdc43d
Dobbymin <gmin0701@knu.ac.kr>
2024-10-01 11:20:47 +0900

    !Hotfix: 병합 오류 해결

--- src/components/features/layout/nav-bar/index.tsx
+++ src/components/features/layout/nav-bar/index.tsx
@@ -15,7 +15,6 @@ const NavBar = () => {
 
   return (
     <Wrapper as='nav'>
-    <Wrapper as='nav'>
       {navBarData.map((item) => (
         <Tab as='a' key={item.title} onClick={() => handleNav(item.path)}>
           <IconImage src={item.icon} alt={item.title} />
